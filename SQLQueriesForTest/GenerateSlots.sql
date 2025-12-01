547113ae8f08607228a15658e16e76011fe5f984
Sina <sinafateh1382@gmail.com>
2025-12-01 16:48:38 +0330

    add 'created_at' and 'updated_at' to query

--- SQLQueriesForTest/GenerateSlots.sql
+++ SQLQueriesForTest/GenerateSlots.sql
@@ -13,17 +13,17 @@ BEGIN
     SET slot_end = CONCAT(CURDATE(), ' 10:00:00');
 
     WHILE day_counter < 7 DO
-        -- حلقه 9 صبح تا 5 عصر هر روز
+        
         WHILE slot_start < CONCAT(DATE_ADD(CURDATE(), INTERVAL day_counter DAY), ' 17:00:00') DO
-            INSERT IGNORE INTO appointments_availableslot (start_time, end_time, is_booked)
-            VALUES (slot_start, slot_end, FALSE);
+            INSERT IGNORE INTO appointments_availableslot 
+            (start_time, end_time, is_booked, created_at, updated_at)
+            VALUES 
+            (slot_start, slot_end, FALSE, NOW(), NOW());
 
-            -- حرکت به اسلات بعدی
             SET slot_start = DATE_ADD(slot_start, INTERVAL 1 HOUR);
             SET slot_end = DATE_ADD(slot_end, INTERVAL 1 HOUR);
         END WHILE;
 
-        -- آماده شدن برای روز بعد
         SET day_counter = day_counter + 1;
         SET slot_start = CONCAT(DATE_ADD(CURDATE(), INTERVAL day_counter DAY), ' 09:00:00');
         SET slot_end = CONCAT(DATE_ADD(CURDATE(), INTERVAL day_counter DAY), ' 10:00:00');
@@ -33,4 +33,3 @@ END$$
 DELIMITER ;
 
 CALL insert_week_slots();
-
